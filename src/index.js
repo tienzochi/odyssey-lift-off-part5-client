2117b4d0f01bf0fbe067958b3b769ea5a3a92612
Rosette Tienzo <tienzorosette@gmail.com>
2022-03-06 17:48:22 +1300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: uri: 'https://server-catstronauts.herokuapp.com/',
+  uri: 'https://server-catstronauts.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
